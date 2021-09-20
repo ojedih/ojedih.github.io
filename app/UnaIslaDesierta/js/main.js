177a971724483ac88de410c093ba149ce123ff0a
oJedih <alejandro.ojedagarcia@gmail.com>
2021-09-20 16:00:34 +0200

    d

--- app/UnaIslaDesierta/js/main.js
+++ app/UnaIslaDesierta/js/main.js
@@ -1,7 +1,13 @@
 $('.loader').hide();
+$('.container').hide();
 
 $('.btn').on("click", function(){
     $('.button-div').fadeOut(700, function(){
-        $('.loader').fadeIn(700);
+        $('.loader').fadeIn(700, function(){
+            $(this).fadeOut(700, function(){
+                $('.container').fadeIn(700)
+            });
+        });
+        
     });
 });
